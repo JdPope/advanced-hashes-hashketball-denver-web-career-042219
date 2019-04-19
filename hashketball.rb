5ccf4ddd3abfbd78e1758d3e9a66f45f3207ee65
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-19 16:55:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -49,7 +49,7 @@ end
 def num_points_scored(name)
   game_hash.each do |location, team_data|
   if team_data[:players].include?name
-    return team_data_[players][name][:points]
+    return team_data[:players][name][:points]
 end
 return 
 end
